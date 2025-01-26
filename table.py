3f13339e41c8475acdf2c4027389def16f1bdccc
Pawan Shrestha <shresthapawan2024@gmail.com>
2025-01-26 23:32:29 +0545

    table

---
+++ table.py
@@ -0,0 +1,7 @@
+#Table from 8 to 10 using for loop
+for b in range(1,11):
+    print("8*",b,"=",8*b)
+for b in range(1,11):
+    print("9*",b,"=",9*b)
+for b in range(1,11):
+    print("10*",b,"=",10*b)
