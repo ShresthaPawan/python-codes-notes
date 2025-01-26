d82328543a8c15574969af96fb99f37fdcccc363
Pawan Shrestha <shresthapawan2024@gmail.com>
2025-01-26 23:50:16 +0545

    string

---
+++ String .py
@@ -0,0 +1,7 @@
+#String Indexing
+word="Hello World"#indexing starts from 0(left to right ) and from -1(right to left)
+print(word[4],word[-8]) #4=o -8=l
+#string slicing (indexing end one step before the actual parameter)
+print(word[0:7])
+print(word[0:9:2])
+print(word[4::3])
