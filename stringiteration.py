24d798f1e4e6c9b543dc8d5b69f0c3684bad6e94
Pawan Shrestha <shresthapawan2024@gmail.com>
2025-01-28 13:27:45 +0545

    iteration complete

--- stringiteration.py
+++ stringiteration.py
@@ -3,5 +3,16 @@ a="Welcome to Nepal"
 b=len(a)
 for c in range(b):
     print(a[c])
+    #without using lens
 for c in a:
     print(c)
+# for reverse iteration
+p="My name is Pawan Shrestha"
+p=p[-1::-1]
+q=len(p)
+for r in range(q):
+    print(p[r]) 
+     #or
+s=len(p)
+for t in range(s-1,-1,-1):
+    print(p[t])
